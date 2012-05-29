d77d23dabd81432726db88d682387ed91659f816
Daniel Prokesch <daniel.prokesch@gmail.com>
2012-05-29 22:23:53 +0200

    pruned PatmosAsmPrinter.cpp

--- lib/Target/Patmos/Patmos.h
+++ lib/Target/Patmos/Patmos.h
@@ -18,19 +18,6 @@
 #include "MCTargetDesc/PatmosMCTargetDesc.h"
 #include "llvm/Target/TargetMachine.h"
 
-namespace PatmosCC {
-  // Patmos specific condition code.
-  enum CondCodes {
-    COND_E  = 0,  // aka COND_Z
-    COND_NE = 1,  // aka COND_NZ
-    COND_HS = 2,  // aka COND_C
-    COND_LO = 3,  // aka COND_NC
-    COND_GE = 4,
-    COND_L  = 5,
-
-    COND_INVALID = -1
-  };
-}
 
 namespace llvm {
   class PatmosTargetMachine;
