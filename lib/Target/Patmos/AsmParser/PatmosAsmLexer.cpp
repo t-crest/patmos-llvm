84a08a63e932cfd20292c89de4ccca503542dae7
Stefan Hepp <stefan@stefant.org>
2012-05-30 11:44:45 +0200

    added asm lexer stub

---
+++ lib/Target/Patmos/AsmParser/PatmosAsmLexer.cpp
@@ -0,0 +1,121 @@
+//===-- PatmosAsmLexer.cpp - Tokenize Patmos assembly to AsmTokens --------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#include "MCTargetDesc/PatmosBaseInfo.h"
+
+#include "llvm/MC/MCAsmInfo.h"
+#include "llvm/MC/MCParser/MCAsmLexer.h"
+#include "llvm/MC/MCParser/MCParsedAsmOperand.h"
+#include "llvm/MC/MCRegisterInfo.h"
+#include "llvm/MC/MCTargetAsmLexer.h"
+
+#include "llvm/Support/TargetRegistry.h"
+
+#include "llvm/ADT/StringSwitch.h"
+
+#include <string>
+#include <map>
+
+using namespace llvm;
+
+namespace {
+
+class PatmosAsmLexer : public MCTargetAsmLexer {
+  const MCAsmInfo &AsmInfo;
+
+  const AsmToken &lexDefinite() {
+    return getLexer()->Lex();
+  }
+
+  AsmToken LexTokenUAL();
+protected:
+  typedef std::map <std::string, unsigned> rmap_ty;
+
+  rmap_ty RegisterMap;
+
+  void InitRegisterMap(const MCRegisterInfo *info) {
+    unsigned numRegs = info->getNumRegs();
+
+    for (unsigned i = 0; i < numRegs; ++i) {
+      const char *regName = info->getName(i);
+      if (regName)
+        RegisterMap[regName] = i;
+    }
+  }
+
+  unsigned MatchRegisterName(StringRef Name) {
+    rmap_ty::iterator iter = RegisterMap.find(Name.str());
+    if (iter != RegisterMap.end())
+      return iter->second;
+    else
+      return 0;
+  }
+
+  AsmToken LexToken() {
+    if (!Lexer) {
+      SetError(SMLoc(), "No MCAsmLexer installed");
+      return AsmToken(AsmToken::Error, "", 0);
+    }
+
+    switch (AsmInfo.getAssemblerDialect()) {
+    default:
+      SetError(SMLoc(), "Unhandled dialect");
+      return AsmToken(AsmToken::Error, "", 0);
+    case 0:
+      return LexTokenUAL();
+    }
+  }
+public:
+  PatmosAsmLexer(const Target &T, const MCRegisterInfo &MRI, const MCAsmInfo &MAI)
+    : MCTargetAsmLexer(T), AsmInfo(MAI) {
+    InitRegisterMap(&MRI);
+  }
+};
+
+} // end anonymous namespace
+
+AsmToken PatmosAsmLexer::LexTokenUAL() {
+  const AsmToken &lexedToken = lexDefinite();
+
+  switch (lexedToken.getKind()) {
+  default: break;
+  case AsmToken::Error:
+    SetError(Lexer->getErrLoc(), Lexer->getErr());
+    break;
+  case AsmToken::Identifier: {
+    std::string lowerCase = lexedToken.getString().lower();
+
+    unsigned regID = MatchRegisterName(lowerCase);
+    // Check for register aliases.
+    //   r13 -> sp
+    //   r14 -> lr
+    //   r15 -> pc
+    //   ip  -> r12
+    //   FIXME: Some assemblers support lots of others. Do we want them all?
+    if (!regID) {
+      regID = StringSwitch<unsigned>(lowerCase)
+        .Case("r30", Patmos::RFP)
+        .Case("r31", Patmos::RSP)
+        .Default(0);
+    }
+
+    if (regID)
+      return AsmToken(AsmToken::Register,
+                      lexedToken.getString(),
+                      static_cast<int64_t>(regID));
+    break;
+  }
+  }
+
+  return AsmToken(lexedToken);
+}
+
+extern "C" void LLVMInitializePatmosAsmLexer() {
+  RegisterMCAsmLexer<PatmosAsmLexer> X(ThePatmosTarget);
+}
--- lib/Target/Patmos/AsmParser/PatmosAsmParser.cpp
+++ lib/Target/Patmos/AsmParser/PatmosAsmParser.cpp
@@ -8,29 +8,50 @@
 //===----------------------------------------------------------------------===//
 
 #include "MCTargetDesc/PatmosMCTargetDesc.h"
-#include "llvm/MC/MCParser/MCAsmLexer.h"
 #include "llvm/MC/MCTargetAsmParser.h"
+#include "llvm/MC/MCParser/MCAsmLexer.h"
+#include "llvm/MC/MCParser/MCAsmParser.h"
+#include "llvm/MC/MCParser/MCParsedAsmOperand.h"
+#include "llvm/MC/MCStreamer.h"
+#include "llvm/MC/MCExpr.h"
+#include "llvm/MC/MCInst.h"
+#include "llvm/MC/MCTargetAsmParser.h"
+#include "llvm/Support/SourceMgr.h"
 #include "llvm/Support/TargetRegistry.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/ADT/SmallVector.h"
+#include "llvm/ADT/Twine.h"
 
 using namespace llvm;
 
 namespace {
 class PatmosAsmParser : public MCTargetAsmParser {
+  MCAsmParser &Parser;
+
+  MCAsmParser &getParser() const { return Parser; }
+  MCAsmLexer &getLexer() const { return Parser.getLexer(); }
+
+  void Warning(SMLoc L, const Twine &Msg) { Parser.Warning(L, Msg); }
+  bool Error(SMLoc L, const Twine &Msg) { return Parser.Error(L, Msg); }
+
   bool MatchAndEmitInstruction(SMLoc IDLoc,
                                SmallVectorImpl<MCParsedAsmOperand*> &Operands,
                                MCStreamer &Out);
 
   bool ParseRegister(unsigned &RegNo, SMLoc &StartLoc, SMLoc &EndLoc);
 
-  bool ParseInstruction(StringRef Name, SMLoc NameLoc,
-                                SmallVectorImpl<MCParsedAsmOperand*> &Operands);
-
-  bool ParseDirective(AsmToken DirectiveID);
+  #define GET_ASSEMBLER_HEADER
+  //#include "PatmosGenAsmMatcher.inc"
 
 public:
   PatmosAsmParser(MCSubtargetInfo &sti, MCAsmParser &parser)
-    : MCTargetAsmParser() {
+    : MCTargetAsmParser(), Parser(parser) {
   }
+
+  virtual bool ParseInstruction(StringRef Name, SMLoc NameLoc,
+                                SmallVectorImpl<MCParsedAsmOperand*> &Operands);
+
+  virtual bool ParseDirective(AsmToken DirectiveID);
 
 };
 }
@@ -58,6 +79,14 @@ ParseDirective(AsmToken DirectiveID) {
   return true;
 }
 
+extern "C" void LLVMInitializePatmosAsmLexer();
+
 extern "C" void LLVMInitializePatmosAsmParser() {
   RegisterMCAsmParser<PatmosAsmParser> X(ThePatmosTarget);
+  LLVMInitializePatmosAsmLexer();
 }
+
+#define GET_REGISTER_MATCHER
+#define GET_MATCHER_IMPLEMENTATION
+//#include "PatmosGenAsmMatcher.inc"
+
