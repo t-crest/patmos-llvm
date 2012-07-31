b657826a798123cfd0c175d81e61bebf682e0dd7
Daniel Prokesch <daniel.prokesch@gmail.com>
2012-07-31 13:50:21 +0200

    float.py support printing decimal for 32bit parts of double value

--- test/CodeGen/Patmos/float.py
+++ test/CodeGen/Patmos/float.py
@@ -17,9 +17,17 @@ def print_info(fmt, flnum):
   # comute decimal
   res = 0
   for byt in tup: res = (res << 8) + byt
+  # 2 parts
+  if len(tup)==8:
+    part1 = part2 = 0
+    for byt in tup[0:4]: part1 = (part1 << 8) + byt
+    for byt in tup[4:8]: part2 = (part2 << 8) + byt
   # output
   print fmt
   print '  Dec:', res
+  if len(tup)==8:
+    print '    bytes 0-3:', part1
+    print '    bytes 4-7:', part2
   print '  Hex:', hex(res)[2:].rjust(len(tup)*2,'0')
   print '  Bin:', bin(res)[2:].rjust(len(tup)*8,'0')
   print '  from', unpack(formats[fmt], packed)[0]
