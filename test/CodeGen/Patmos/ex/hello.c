91396daee1ec40aa445379b6c73bacb8dd2beee7
Stefan Hepp <stefan@stefant.org>
2012-08-28 16:41:06 +0200

    cleanup readme and hello.c example

--- test/CodeGen/Patmos/ex/hello.c
+++ test/CodeGen/Patmos/ex/hello.c
@@ -1,22 +1,8 @@
 #include <stdio.h>
 
-void test(const char *s, int i) {
-    int j = i / 3;
-    float f = 0.5f;
-    float k = f / 2.0f;
-    printf("%s : %d %f\n", s, j, k);
-}
-
 int main(int argc, char** argv) {
-    const char *s;
 
-    if (argc < 2) {
-	s = "Hello World!";
-    } else {
-	s = "Goodbye";
-    }
-
-    test(s, 8 + argc);
+    printf("Hello World: %d\n", argc);
 
     return 0;
 }
