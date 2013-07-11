62c3592c384eb6b886251f02618835b17a08dd8c
Benedikt Huber <benedikt.huber@gmail.com>
2013-07-11 13:09:36 +0200

    platin: improve install script (automated installation of dependencies)

--- tools/platin/install.sh
+++ tools/platin/install.sh
@@ -1,46 +1,142 @@
 #!/bin/bash
 #
+# platin installation script
+#
+
 SRC_DIR=$(dirname "${0}")
 
-INSTALL_DIR=$1
+function usage() {
+    echo "usage: ${0} -i INSTALL_DIR [-d]" >&2
+    echo >&2
+    echo "Install the platin toolchain relative to the given prefix" >&2
+    echo "Installed Files: " >&2
+    echo "  <prefix>/bin/platin      ... Binary" >&2
+    echo "  <prefix>/lib/platin/     ... Library Directory" >&2
+    echo "  <prefix>/lib/platin/gems ... Ruby Libraries (if necessary)" >&2
+    echo "OPTIONS" >&2
+    echo "  -d     dry run" >&2
+    echo "  -v     verbose" >&2
+    exit 1
+
+}
+
+while getopts "hi:dvx" opt; do
+  case $opt in
+    h) usage; exit 0 ;;
+    i) INSTALL_DIR="${OPTARG}" ;;
+    d) DRYRUN=true; VERBOSE=true ;;
+    v) VERBOSE=true ;;
+    x) set -x ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      usage >&2
+      exit 1
+      ;;
+  esac
+done
+
 if [ -z "${INSTALL_DIR}" ] ; then
-echo "usage: ${0} <prefix>" >&2
-echo >&2
-echo "Install the platin toolchain relative to the given prefix" >&2
-echo "If INSTALL_GEMS is set, the ruby libraries are installed as well" >&2
-echo "Installed Files: " >&2
-echo "  <prefix>/bin/platin  ... Binary" >&2
-echo "  <prefix>/lib/platin/ ... Library Directory" >&2
-exit 1
+    usage
 fi
 
 BINARY="${INSTALL_DIR}/bin/platin"
-GEM_DIR="${INSTALL_DIR}/lib/gems/1.9"
-echo "${BINARY}"
-mkdir -p "$(dirname "${BINARY}")"
-cat <<EOF >"${BINARY}"
+LIB_DIR="${INSTALL_DIR}/lib/platin"
+GEM_DIR="${LIB_DIR}/gems"
+
+function info() {
+    if [ ! -z "${VERBOSE}" ] ; then
+	echo 'INFO' "${@}"
+    else
+	echo "${@}"
+    fi
+}
+
+function install() {
+    DST="${1}"
+    SRC="${2}"
+    info "Installing ${SRC} -> ${DST}"
+    run mkdir -p "$(dirname "${DST}")"
+    run cp "${SRC}" "${DST}"
+}
+
+function verbose() {
+    if [ ! -z "${VERBOSE}" ] ; then
+	echo "${@}"
+    fi
+}
+function run() {
+    CMD="${1}"
+    shift
+    verbose "${CMD}" "${@}"
+    if [ -z "${DRYRUN}" ] ; then
+	"${CMD}" "${@}"
+    fi
+}
+
+function install_binary() {
+    install "${BINARY}" "${SRC_DIR}/platin"
+    if [ -z "${DRYRUN}" ] ; then
+	cat <<EOF >"${BINARY}"
 #!/bin/bash
 
 # Installer
 RELATIVE_LIBDIR="../lib/platin"
 
 # Local Gems
-export GEM_PATH="${GEM_DIR}:${GEM_PATH}"
-
+if [ -z "\${GEM_PATH}" ] ; then
+  export GEM_PATH="${GEM_DIR}"
+else
+  export GEM_PATH="${GEM_DIR}:\${GEM_PATH}"
+fi
 # Executable
 EOF
-cat  "${SRC_DIR}/platin"  >> "${BINARY}"
-chmod uga+x "${BINARY}"
+	cat "${SRC_DIR}/platin"  >> "${BINARY}"
+    fi
+    run chmod uga+x "${BINARY}"
+}
+
+install_binary
 
 for libfile in $(cd "${SRC_DIR}/lib" ; find "." -name '*.rb') ; do
     SRC="${SRC_DIR}/lib/${libfile}"
     DST="${INSTALL_DIR}/lib/platin/${libfile}"
-    echo "${DST}"
-    mkdir -p "$(dirname "${DST}")"
-    cp "${SRC}" "${DST}"
+    install "${DST}" "${SRC}"
 done
 
-if [ ! -z "${INSTALL_GEMS}" ] ; then
-    echo "Installing ruby gems"
-    "${SRC_DIR}"/ext/install_gems.sh "${GEM_DIR}"
+function detect_gem_command {
+    GEM19s="gem1.9.1 gem1.9 gem"
+    for g19 in ${GEM19s} ; do
+        if [ ! -z "`which ${g19}  2>/dev/null`" ] ; then
+            GEM="$g19"
+            return
+        fi
+    done
+}
+
+detect_gem_command
+if [ ! -z "${GEM}" ] ; then
+    if [ -z "${GEM_PATH}" ] ; then
+	export GEM_PATH="${GEM_DIR}"
+    else
+	export GEM_PATH="${GEM_DIR}:${GEM_PATH}"
+    fi
+    for gemqname in rsec:0.4 ruby-graphviz:1.0.8 lpsolve:5.5.10.j ; do
+	gemname=$(echo "${gemqname}" | cut -d':' -f1)
+	gemversion=$(echo "${gemqname}" | cut -d':' -f2)
+	verbose "INFO checking for gem ${gemqname}"
+	if [ "$(${GEM} list ${gemname} --version ${gemversion} -i)" != "true" ] ; then
+	    info "Installing gem ${gemname} --version ${gemversion} (missing)"
+	    local_gemfile="${SRC_DIR}/ext/${gemname}-${gemversion}.gem"
+	    if [ -e "${local_gemfile}" ] ; then
+		run "${GEM}" install --install-dir "${GEM_DIR}" "${local_gemfile}" -q
+	    else
+		run "${GEM}" install --install-dir "${GEM_DIR}" "${gemname}" --version "${gemversion}" -q
+	    fi
+	    if [ "${?}" -ne 0 ] ; then
+		echo "ERROR: failed to install gem ${gemname} --version ${gemversion}" >&2
+	    fi
+	fi
+    done	
+else
+    echo "WARNING: did not find 'gem' command; skipping ruby library checks" >&2
 fi
