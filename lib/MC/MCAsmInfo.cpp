ee0f01f6bf329ce959caf5abb6bfaf7d2ce3c3da
David Fang <fang@csl.cornell.edu>
2013-12-10 21:37:41 +0000

    on darwin<10, fallback to .weak_definition (PPC,X86) .weak_def_can_be_hidden was not yet supported by the system assembler
    git-svn-id: https://llvm.org/svn/llvm-project/llvm/trunk@196970 91177308-0d34-0410-b5e6-96231b3b80d8

--- lib/MC/MCAsmInfo.cpp
+++ lib/MC/MCAsmInfo.cpp
@@ -79,6 +79,7 @@ MCAsmInfo::MCAsmInfo() {
   HasNoDeadStrip = false;
   WeakRefDirective = 0;
   HasWeakDefDirective = false;
+  HasWeakDefCanBeHiddenDirective = false;
   HasLinkOnceDirective = false;
   HiddenVisibilityAttr = MCSA_Hidden;
   HiddenDeclarationVisibilityAttr = MCSA_Hidden;
