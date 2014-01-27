ac82a0fd46bb3ed54cbdfc2e05e4ed3a7d3dcb49
Stepan Dyatkovskiy <stpworld@narod.ru>
2014-01-27 09:18:31 +0000

    Fix for PR18102.
    Issue outcomes from DAGCombiner::MergeConsequtiveStores, more precisely from
    mem-ops sequence sorting.
    
    Consider, how MergeConsequtiveStores works for next example:
    
    store i8 1, a[0]
    store i8 2, a[1]
    store i8 3, a[1]   ; a[1] again.
    return   ; DAG starts here
    
    1. Method will collect all the 3 stores.
    2. It sorts them by distance from the base pointer (farthest with highest
    index).
    3. It takes first consecutive non-overlapping stores and (if possible) replaces
    them with a single store instruction.
    
    The point is, we can't determine here which 'store' instruction
    would be the second after sorting ('store 2' or 'store 3').
    It happens that 'store 3' would be the second, and 'store 2' would be the third.
    
    So after merging we have the next result:
    
    store i16 (1 | 3 << 8), base   ; is a[0] but bit-casted to i16
    store i8 2, a[1]
    
    So actually we swapped 'store 3' and 'store 2' and got wrong contents in a[1].
    
    Fix: In sort routine just also take into account mem-op sequence number.
    
    git-svn-id: https://llvm.org/svn/llvm-project/llvm/trunk@200201 91177308-0d34-0410-b5e6-96231b3b80d8

--- lib/CodeGen/SelectionDAG/DAGCombiner.cpp
+++ lib/CodeGen/SelectionDAG/DAGCombiner.cpp
@@ -8760,7 +8760,10 @@ struct MemOpLink {
 // base ptr.
 struct ConsecutiveMemoryChainSorter {
   bool operator()(MemOpLink LHS, MemOpLink RHS) {
-    return LHS.OffsetFromBase < RHS.OffsetFromBase;
+    return
+        LHS.OffsetFromBase < RHS.OffsetFromBase ||
+        (LHS.OffsetFromBase == RHS.OffsetFromBase &&
+         LHS.SequenceNum > RHS.SequenceNum);
   }
 };
 
