8e4c5163e4ea6eda52edabc0107b1ce795b8d08f
Saleem Abdulrasool <compnerd@compnerd.org>
2014-03-13 07:02:46 +0000

    MC: fix silly typo
    git-svn-id: https://llvm.org/svn/llvm-project/llvm/trunk@203763 91177308-0d34-0410-b5e6-96231b3b80d8

--- lib/MC/MCParser/AsmParser.cpp
+++ lib/MC/MCParser/AsmParser.cpp
@@ -2029,7 +2029,7 @@ bool AsmParser::parseMacroArguments(const MCAsmMacro *M,
       if (FAI >= NParameters) {
         Error(IDLoc,
               "parameter named '" + FA.Name + "' does not exist for macro '" +
-              (M ? M->Name : "<unamed>") + "'");
+              (M ? M->Name : "<unnamed>") + "'");
         return true;
       }
       PI = FAI;
