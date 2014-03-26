07ee31e6448bbb5127603a3bcdb33d8457ca6668
Renato Golin <renato.golin@linaro.org>
2014-03-26 12:52:28 +0000

    Add @llvm.clear_cache builtin
    Implementing the LLVM part of the call to __builtin___clear_cache
    which translates into an intrinsic @llvm.clear_cache and is lowered
    by each target, either to a call to __clear_cache or nothing at all
    incase the caches are unified.
    
    Updating LangRef and adding some tests for the implemented architectures.
    Other archs will have to implement the method in case this builtin
    has to be compiled for it, since the default behaviour is to bail
    unimplemented.
    
    A Clang patch is required for the builtin to be lowered into the
    llvm intrinsic. This will be done next.
    
    git-svn-id: https://llvm.org/svn/llvm-project/llvm/trunk@204802 91177308-0d34-0410-b5e6-96231b3b80d8

--- lib/CodeGen/SelectionDAG/SelectionDAGBuilder.cpp
+++ lib/CodeGen/SelectionDAG/SelectionDAGBuilder.cpp
@@ -5393,6 +5393,8 @@ SelectionDAGBuilder::visitIntrinsicCall(const CallInst &I, unsigned Intrinsic) {
     (void)getControlRoot();
     return 0;
   }
+  case Intrinsic::clear_cache:
+    return TLI->getClearCacheBuiltinName();
   case Intrinsic::donothing:
     // ignore
     return 0;
