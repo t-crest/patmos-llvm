864a4b900e3d9f978414226d83506d670e787689
Gerolf Hoflehner <ghoflehner@apple.com>
2014-08-03 21:35:39 +0000

    MachineCombiner Pass for selecting faster instruction  sequence -  target independent framework
     When the DAGcombiner selects instruction sequences
     it could increase the critical path or resource len.
    
     For example, on arm64 there are multiply-accumulate instructions (madd,
     msub). If e.g. the equivalent  multiply-add sequence is not on the
     crictial path it makes sense to select it instead of  the combined,
     single accumulate instruction (madd/msub). The reason is that the
     conversion from add+mul to the madd could lengthen the critical path
     by the latency of the multiply.
    
     But the DAGCombiner would always combine and select the madd/msub
     instruction.
    
     This patch uses machine trace metrics to estimate critical path length
     and resource length of an original instruction sequence vs a combined
     instruction sequence and picks the faster code based on its estimates.
    
     This patch only commits the target independent framework that evaluates
     and selects code sequences. The machine instruction combiner is turned
     off for all targets and expected to evolve over time by gradually
     handling DAGCombiner pattern in the target specific code.
    
     This framework lays the groundwork for fixing
     rdar://16319955
    
    git-svn-id: https://llvm.org/svn/llvm-project/llvm/trunk@214666 91177308-0d34-0410-b5e6-96231b3b80d8

--- include/llvm/InitializePasses.h
+++ include/llvm/InitializePasses.h
@@ -280,6 +280,7 @@ void initializeSLPVectorizerPass(PassRegistry&);
 void initializeBBVectorizePass(PassRegistry&);
 void initializeMachineFunctionPrinterPassPass(PassRegistry&);
 void initializeStackMapLivenessPass(PassRegistry&);
+void initializeMachineCombinerPass(PassRegistry &);
 void initializeLoadCombinePass(PassRegistry&);
 }
 
