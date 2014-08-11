d9453d40d6a4b1e415e039a65925c39b2e8fbcd3
Hans Wennborg <hans@hanshq.net>
2014-08-11 02:17:15 +0000

    Increase the size of PMTopLevelManager::ImmutablePasses from 8 to 16.
    During a bootstrap build of Clang, this SmallVector always held 14 elements.
    
    git-svn-id: https://llvm.org/svn/llvm-project/llvm/trunk@215333 91177308-0d34-0410-b5e6-96231b3b80d8

--- include/llvm/IR/LegacyPassManagers.h
+++ include/llvm/IR/LegacyPassManagers.h
@@ -258,7 +258,7 @@ private:
   DenseMap<Pass *, SmallPtrSet<Pass *, 8> > InversedLastUser;
 
   /// Immutable passes are managed by top level manager.
-  SmallVector<ImmutablePass *, 8> ImmutablePasses;
+  SmallVector<ImmutablePass *, 16> ImmutablePasses;
 
   /// TopLevelManagers from which ImmutablePasses are inherited
   SmallVector<PMTopLevelManager *, 4> InheritedPassManagers;
