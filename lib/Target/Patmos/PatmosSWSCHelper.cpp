e946a1e26c577b414a4e8fd99090e8b925ebd6cb
Alexander Jordan <ajordan@complang.tuwien.ac.at>
2014-10-20 14:31:01 +0200

    renamed file for stack cache helper pass

--- lib/Target/Patmos/PatmosSWSCHelper.cpp
+++ lib/Target/Patmos/PatmosSWSCHelper.cpp
@@ -20,66 +20,6 @@
 
 using namespace llvm;
 
-
-// anonymous namespace
-namespace {
-
-  class PatmosRmLoopbound : public MachineFunctionPass {
-  private:
-    /// Pass ID
-    static char ID;
-
-    const PatmosTargetMachine &TM;
-    const PatmosSubtarget &STC;
-    const PatmosInstrInfo *TII;
-
-
-  public:
-    PatmosRmLoopbound(const PatmosTargetMachine &tm) :
-      MachineFunctionPass(ID), TM(tm),
-      STC(tm.getSubtarget<PatmosSubtarget>()),
-        TII(static_cast<const PatmosInstrInfo*>(tm.getInstrInfo())) {
-      (void) TM;
-    }
-
-    /// getPassName - Return the pass' name.
-    virtual const char *getPassName() const {
-      return "Patmos Remove Loopbound Pseudos";
-    }
-
-    virtual bool runOnMachineFunction(MachineFunction &MF) {
-      bool changed = false;
-      for (MachineFunction::iterator FI = MF.begin(), FE = MF.end();
-           FI != FE; ++FI) {
-        for (MachineBasicBlock::iterator MI = FI->begin(), ME = FI->end();
-            MI != ME;) {
-          MachineInstr *M = MI;
-          switch (M->getOpcode()) {
-            case Patmos::PSEUDO_LOOPBOUND:
-              ++MI;
-              M->eraseFromParent();
-              changed = true;
-              continue;
-            default: ;//NOP
-          }
-          ++MI;
-        }
-      }
-      return changed;
-    }
-  };
-
-  char PatmosRmLoopbound::ID = 0;
-} // end of anonymous namespace
-
-///////////////////////////////////////////////////////////////////////////////
-
-/// createPatmosRmLoopboundPass - Returns a new PatmosRmLoopbound
-/// \see PatmosRmLoopbound
-FunctionPass *llvm::createPatmosRmLoopboundPass(const PatmosTargetMachine &tm) {
-  return new PatmosRmLoopbound(tm);
-}
-
 // anonymous namespace
 namespace {
 
