0084ff2ad7273ab347d68ab0e2b10b56b7bca475
Matt Arsenault <Matthew.Arsenault@amd.com>
2015-04-23 23:34:51 +0000

    Revert accidentally committed "MC: Allow targets to stop symbol name quoting"
    git-svn-id: https://llvm.org/svn/llvm-project/llvm/trunk@235672 91177308-0d34-0410-b5e6-96231b3b80d8

--- lib/MC/MCContext.cpp
+++ lib/MC/MCContext.cpp
@@ -126,8 +126,7 @@ MCSymbol *MCContext::getOrCreateSectionSymbol(const MCSectionELF &Section) {
   }
 
   auto NameIter = UsedNames.insert(std::make_pair(Name, true)).first;
-  Sym = new (*this) MCSymbol(NameIter->getKey(), /*isTemporary*/ false,
-                             MAI->noSymbolNameQuoting());
+  Sym = new (*this) MCSymbol(NameIter->getKey(), /*isTemporary*/ false);
 
   if (!OldSym)
     OldSym = Sym;
@@ -165,8 +164,7 @@ MCSymbol *MCContext::CreateSymbol(StringRef Name, bool AlwaysAddSuffix) {
       // Ok, we found a name. Have the MCSymbol object itself refer to the copy
       // of the string that is embedded in the UsedNames entry.
       MCSymbol *Result =
-        new (*this) MCSymbol(NameEntry.first->getKey(), IsTemporary,
-                             MAI->noSymbolNameQuoting());
+          new (*this) MCSymbol(NameEntry.first->getKey(), IsTemporary);
       return Result;
     }
     assert(IsTemporary && "Cannot rename non-temporary symbols");
