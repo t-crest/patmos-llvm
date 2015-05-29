ca0b13732390e79463e394608debf92620f59a90
Rafael Espindola <rafael.espindola@gmail.com>
2015-05-29 19:07:51 +0000

    Move Flags from MCSymbolData to MCSymbol.
    git-svn-id: https://llvm.org/svn/llvm-project/llvm/trunk@238598 91177308-0d34-0410-b5e6-96231b3b80d8

--- lib/MC/MCAssembler.cpp
+++ lib/MC/MCAssembler.cpp
@@ -1201,7 +1201,6 @@ void MCSymbolData::dump() const {
 
   OS << "<MCSymbolData"
      << " Fragment:" << getFragment();
-  OS << " Flags:" << getFlags();
   if (isExternal())
     OS << " (external)";
   if (isPrivateExtern())
