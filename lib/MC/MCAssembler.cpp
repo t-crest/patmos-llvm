ba7cba25b42f5841863f429da86d269c98360241
Matt Arsenault <Matthew.Arsenault@amd.com>
2015-06-09 00:31:39 +0000

    MC: Add target hook to control symbol quoting
    git-svn-id: https://llvm.org/svn/llvm-project/llvm/trunk@239370 91177308-0d34-0410-b5e6-96231b3b80d8

--- lib/MC/MCAssembler.cpp
+++ lib/MC/MCAssembler.cpp
@@ -1212,8 +1212,7 @@ void MCFragment::dump() {
   case MCFragment::FT_SafeSEH: {
     const MCSafeSEHFragment *F = cast<MCSafeSEHFragment>(this);
     OS << "\n       ";
-    OS << " Sym:";
-    F->getSymbol()->print(OS);
+    OS << " Sym:" << F->getSymbol();
     break;
   }
   }
