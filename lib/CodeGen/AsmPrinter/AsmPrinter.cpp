82403011b5c203b14fa7a62dffa37714bb977979
Reid Kleckner <reid@kleckner.net>
2015-07-07 22:25:32 +0000

    Rename llvm.frameescape and llvm.framerecover to localescape and localrecover
    Summary:
    Initially, these intrinsics seemed like part of a family of "frame"
    related intrinsics, but now I think that's more confusing than helpful.
    Initially, the LangRef specified that this would create a new kind of
    allocation that would be allocated at a fixed offset from the frame
    pointer (EBP/RBP). We ended up dropping that design, and leaving the
    stack frame layout alone.
    
    These intrinsics are really about sharing local stack allocations, not
    frame pointers. I intend to go further and add an `llvm.localaddress()`
    intrinsic that returns whatever register (EBP, ESI, ESP, RBX) is being
    used to address locals, which should not be confused with the frame
    pointer.
    
    Naming suggestions at this point are welcome, I'm happy to re-run sed.
    
    Reviewers: majnemer, nicholas
    
    Subscribers: llvm-commits
    
    Differential Revision: http://reviews.llvm.org/D11011
    
    git-svn-id: https://llvm.org/svn/llvm-project/llvm/trunk@241633 91177308-0d34-0410-b5e6-96231b3b80d8

--- lib/CodeGen/AsmPrinter/AsmPrinter.cpp
+++ lib/CodeGen/AsmPrinter/AsmPrinter.cpp
@@ -823,7 +823,7 @@ void AsmPrinter::EmitFunctionBody() {
         emitCFIInstruction(MI);
         break;
 
-      case TargetOpcode::FRAME_ALLOC:
+      case TargetOpcode::LOCAL_ESCAPE:
         emitFrameAlloc(MI);
         break;
 
