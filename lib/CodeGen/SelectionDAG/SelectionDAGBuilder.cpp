b0227c508cb7be8bdca97491f9d24b8357ccc4d9
Mehdi Amini <mehdi.amini@apple.com>
2015-07-09 02:09:28 +0000

    Make getByValTypeAlignment() taking DataLayout as an argument
    Summary:
    This change is part of a series of commits dedicated to have a single
    DataLayout during compilation by using always the one owned by the
    module.
    
    Reviewers: echristo
    
    Subscribers: yaron.keren, rafael, llvm-commits, jholewinski
    
    Differential Revision: http://reviews.llvm.org/D11038
    
    From: Mehdi Amini <mehdi.amini@apple.com>
    
    git-svn-id: https://llvm.org/svn/llvm-project/llvm/trunk@241777 91177308-0d34-0410-b5e6-96231b3b80d8

--- lib/CodeGen/SelectionDAG/SelectionDAGBuilder.cpp
+++ lib/CodeGen/SelectionDAG/SelectionDAGBuilder.cpp
@@ -6885,7 +6885,7 @@ TargetLowering::LowerCallTo(TargetLowering::CallLoweringInfo &CLI) const {
         if (Args[i].Alignment)
           FrameAlign = Args[i].Alignment;
         else
-          FrameAlign = getByValTypeAlignment(ElementTy);
+          FrameAlign = getByValTypeAlignment(ElementTy, DL);
         Flags.setByValAlign(FrameAlign);
       }
       if (Args[i].isNest)
@@ -7156,7 +7156,7 @@ void SelectionDAGISel::LowerArguments(const Function &F) {
         if (F.getParamAlignment(Idx))
           FrameAlign = F.getParamAlignment(Idx);
         else
-          FrameAlign = TLI->getByValTypeAlignment(ElementTy);
+          FrameAlign = TLI->getByValTypeAlignment(ElementTy, DL);
         Flags.setByValAlign(FrameAlign);
       }
       if (F.getAttributes().hasAttribute(Idx, Attribute::Nest))
