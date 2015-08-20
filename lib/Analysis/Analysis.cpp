685d00ee9e0feea3b561ba63a1d63aa143bd216a
Chandler Carruth <chandlerc@gmail.com>
2015-08-20 08:06:03 +0000

    [ARC] Pull the ObjC ARC components that really serve the role of analyses into LLVM's Analysis library rather than having them in a Transforms library.
    This is motivated by the need to have the core AliasAnalysis
    infrastructure be aware of the ObjCARCAliasAnalysis. However, it also
    seems like a nice and clean separation. Everything was very easy to move
    and this doesn't create much clutter in the analysis library IMO.
    
    Differential Revision: http://reviews.llvm.org/D12133
    
    git-svn-id: https://llvm.org/svn/llvm-project/llvm/trunk@245541 91177308-0d34-0410-b5e6-96231b3b80d8

--- lib/Analysis/Analysis.cpp
+++ lib/Analysis/Analysis.cpp
@@ -62,6 +62,7 @@ void llvm::initializeAnalysis(PassRegistry &Registry) {
   initializeMemDerefPrinterPass(Registry);
   initializeMemoryDependenceAnalysisPass(Registry);
   initializeModuleDebugInfoPrinterPass(Registry);
+  initializeObjCARCAliasAnalysisPass(Registry);
   initializePostDominatorTreePass(Registry);
   initializeRegionInfoPassPass(Registry);
   initializeRegionViewerPass(Registry);
