3fe6574f904777447e696d55a77bf1a096e927da
Emad Jacob Maroun <emoun.open@gmail.com>
2020-06-18 09:59:59 +0200

    Refactored CI scripts.

--- ci/build_test_llvm.sh
+++ ci/build_test_llvm.sh
@@ -1,32 +1,43 @@
 #!/bin/sh -x
 # Build LLVM and runs automatic tests
 
+CLANG_LINK=https://github.com/t-crest/patmos-clang/
+NEWLIB_LINK=https://github.com/t-crest/patmos-newlib/
+
+CLANG_COMMIT=73474a8ae98f1a281a03c440a4d8b9987029cf4e
+NEWLIB_COMMIT=4c149a53f8cb2478d99aac731b61b5e4ed63543f
+
+PATMOS_TRIPLE=patmos-unknown-unknown-elf
+
+BUILD_DIR=$TRAVIS_BUILD_DIR/build
+PATH_DIR=$BUILD_DIR/local
+
 # Download clang
 cd tools
-git clone https://github.com/t-crest/patmos-clang/ clang
+git clone $CLANG_LINK clang
 cd clang
-git checkout 73474a8ae98f1a281a03c440a4d8b9987029cf4e
+git checkout $CLANG_COMMIT
 cd ../..
 
 # Build LLVM
 cd build
-cmake .. -DCMAKE_CXX_STANDARD=14 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=local -DLLVM_TARGETS_TO_BUILD=Patmos -DLLVM_DEFAULT_TARGET_TRIPLE=patmos-unknown-unknown-elf -DCLANG_ENABLE_ARCMT=false -DCLANG_ENABLE_STATIC_ANALYZER=false -DCLANG_ENABLE_TESTS=false -DCLANG_ENABLE_DOCS=false -DCLANG_BUILD_EXAMPLES=false
+cmake .. -DCMAKE_CXX_STANDARD=14 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=local -DLLVM_TARGETS_TO_BUILD=Patmos -DLLVM_DEFAULT_TARGET_TRIPLE=$PATMOS_TRIPLE -DCLANG_ENABLE_ARCMT=false -DCLANG_ENABLE_STATIC_ANALYZER=false -DCLANG_ENABLE_TESTS=false -DCLANG_ENABLE_DOCS=false -DCLANG_BUILD_EXAMPLES=false
 make UnitTests llc llvm-link clang llvm-config llvm-objdump opt 
 
 # Build newlib
-git clone https://github.com/t-crest/patmos-newlib
+git clone $NEWLIB_LINK
 cd patmos-newlib
-git checkout 4c149a53f8cb2478d99aac731b61b5e4ed63543f
+git checkout $NEWLIB_COMMIT
 mkdir build
 cd build
-../configure  --target=patmos-unknown-unknown-elf AR_FOR_TARGET=ar RANLIB_FOR_TARGET=ranlib LD_FOR_TARGET=ld CC_FOR_TARGET=$TRAVIS_BUILD_DIR/build/bin/clang CFLAGS_FOR_TARGET="-target patmos-unknown-unknown-elf -O3" --prefix=$TRAVIS_BUILD_DIR/build/local
+../configure  --target=$PATMOS_TRIPLE AR_FOR_TARGET=ar RANLIB_FOR_TARGET=ranlib LD_FOR_TARGET=ld CC_FOR_TARGET=$BUILD_DIR/bin/clang CFLAGS_FOR_TARGET="-target $PATMOS_TRIPLE -O3" --prefix=$PATH_DIR
 # We use 'MAKEINFO=true' to avoid building documentation
 make -j MAKEINFO=true
 make -j MAKEINFO=true install
 cd ../..
 
 # Run tests
-env DEBUG_TYPE="" LINK_LIBS=$TRAVIS_BUILD_DIR/build/local/patmos-unknown-unknown-elf/lib ./bin/llvm-lit ../test --filter=Patmos -v
+env DEBUG_TYPE="" LINK_LIBS=$PATH_DIR/$PATMOS_TRIPLE/lib ./bin/llvm-lit ../test --filter=Patmos -v
 
 # Prepare release artifact and put it in cache
 make box
--- ci/run_test_suite.sh
+++ ci/run_test_suite.sh
@@ -1,7 +1,12 @@
 #!/bin/sh -x
 # Runs the test suite for testing LLVM
 
-cd build/bench/build
-ctest -j$(grep -c ^processor /proc/cpuinfo)
-cd ../../..
-rm -rf build/bench
+BENCH_DIR=build/bench
+BENCH_BUILD_DIR=$BENCH_DIR/build
+
+CORE_COUNT=$(grep -c ^processor /proc/cpuinfo)
+
+cd $BENCH_BUILD_DIR
+ctest -j$CORE_COUNT
+cd -
+rm -rf $BENCH_DIR
